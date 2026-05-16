05d5b9072692635a3269414bcc2481ff292d85fe
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2026-05-16 22:15:11 +0530

    commit

---
+++ assembler/test copy.asm
@@ -0,0 +1,9 @@
+MOVI R7, 0   ; R7 = 0
+MOVI R2, 3   ; R2 = 3 (Counter)
+MOVI R3, 1   ; R3 = 1
+NEG R3       ; R3 = -1
+LOOP:
+    ADD R7, R2   ; R7 = R7 + R2
+    ADD R2, R3   ; R2 = R2 - 1
+INF: JZR R2, INF    ; If R2 = 0, Infinite Loop
+JZR R0, LOOP    ; Jump to line 5 (Repeat loop)
